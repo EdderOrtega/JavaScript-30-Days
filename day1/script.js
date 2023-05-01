08df42f58d60c8087aaa238a80f22b12aa204805
Edder <114453344+EdderOrtega@users.noreply.github.com>
2023-04-30 21:44:29 -0600

    reto 1 terminado

---
+++ day1/script.js
@@ -0,0 +1,48 @@
+//objeto con todas las teclas y sus sonidos
+let sonidos = {   
+    "a": "./sounds/clap.wav",
+    "s": "./sounds/hihat.wav",
+    "d": "./sounds/kick.wav",
+    "e": "./sounds/openhat.wav",
+    "g": "./sounds/boom.wav",
+    "h": "./sounds/ride.wav",
+    "j": "./sounds/snare.wav",
+    "k": "./sounds/tom.wav",
+    "l": "./sounds/tink.wav",
+}
+
+//función para reproducir el sonido con el evento
+function reproducirSonido(e) {
+    //declarar variable de audio para acceder a la tecla del objeto presionada mediante el evento
+    const audio = new Audio(sonidos[e.key]);
+    //reproducir audio
+    audio.play();
+    //aqui se obtiene el elemento html h1 de la tecla presionada
+    const tecla = document.querySelector(`.tecla h1[data-key="${e.key}"]`);
+    //si la tecla es correcta 
+    if (tecla) {
+        //aqui se agrega la clase "play" al padre de h1 o sea "tecla"
+        tecla.parentNode.classList.add("play");
+        //se asigna el contenedor "tecla" a una nueva variable
+        const teclaDiv = tecla.parentElement;
+        //se agrega la clase "play"
+        teclaDiv.classList.add("play");
+        //un setTimeout para remover el estilo "play"
+        setTimeout(() => {
+            //removemos la clase "play"
+            teclaDiv.classList.remove("play");
+            //despues de 500 milsegundos
+        }, 500);
+    } 
+    else{
+        //si presiona otra tecla que no tiene sonido sale este log
+        console.log("No se puede reproducir el sonido");
+    }
+}
+//agregando el listen del evento cuando una tecla es presionada
+window.addEventListener("keydown",(e)=>{ 
+    //imprimiendo la tecla presionada
+    console.log(e)
+    //llamando a la función que reproducira el sonido
+    reproducirSonido(e);
+});
